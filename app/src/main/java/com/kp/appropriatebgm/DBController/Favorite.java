edaf2718cc34271d96deffe73eb80cb283cb8fb5
KyungPyo <leepyo116@gmail.com>
2016-02-01 15:58:34 +0900

    DBManager - getFavoriteList + 주석 Favorite Class (수정)

--- app/src/main/java/com/kp/appropriatebgm/DBController/Favorite.java
+++ app/src/main/java/com/kp/appropriatebgm/DBController/Favorite.java
@@ -6,14 +6,12 @@ package com.kp.appropriatebgm.DBController;
 public class Favorite {
     private int favoriteId;
     private String bgmPath;
+    private String bgmName;
 
-    public Favorite(int favoriteId, String bgmPath) {
+    public Favorite(int favoriteId, String bgmPath, String bgmName) {
         this.favoriteId = favoriteId;
-        if (bgmPath.equals("")){
-            this.bgmPath = null;
-        } else {
-            this.bgmPath = bgmPath;
-        }
+        this.bgmPath = bgmPath;
+        this.bgmName = bgmName;
     }
 
     public void setFavoriteId(int favoriteId) {
@@ -22,11 +20,17 @@ public class Favorite {
     public void setBgmPath(String bgmPath) {
         this.bgmPath = bgmPath;
     }
+    public void setBgmName(String bgmName) {
+        this.bgmName = bgmName;
+    }
 
     public int getFavoriteId() {
         return favoriteId;
     }
     public String getBgmPath() {
         return bgmPath;
+    }
+    public String getBgmName() {
+        return bgmName;
     }
 }
