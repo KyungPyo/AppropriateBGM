40bcaea18793fe72e61f3a8fb95464f7186ed46f
choijaesun1 <choijaesun1@gmail.com>
2016-02-23 14:53:35 +0900

    락스크린 통화 시 안뜨게 함.

--- app/src/main/java/com/kp/appropriatebgm/LockScreen/LockScreenReceiver.java
+++ app/src/main/java/com/kp/appropriatebgm/LockScreen/LockScreenReceiver.java
@@ -4,12 +4,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
 /**
  * Created by GD on 2016-02-11.
  */
 public class LockScreenReceiver extends BroadcastReceiver {
 
+    private boolean phoneState = false;
 
     // Method : 잠금화면 브로드캐스트 리시브
     // Return value : void
@@ -20,16 +23,27 @@ public class LockScreenReceiver extends BroadcastReceiver {
         SharedPreferences preferences = context.getSharedPreferences("AppSetting",
                 Context.MODE_PRIVATE);
 
+        String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+
+        if (state != null && (TelephonyManager.EXTRA_STATE_OFFHOOK.equals(state)
+                || intent.getAction().equals(Intent.ACTION_NEW_OUTGOING_CALL))) {
+            phoneState = true;
+        } else if (state != null && TelephonyManager.EXTRA_STATE_IDLE.equals(state)) {
+            phoneState = false;
+        }
+
         if (preferences.getBoolean("LockerOn", false)) {
             if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
                 Intent i = new Intent(context, LockScreenService.class);
                 context.startService(i);
-            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF) && !phoneState) {// 통화중이 아닐때
+
                 Intent in = new Intent(context, LockScreenActivity.class);
                 in.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 in.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
 
                 context.startActivity(in);
+
             }
         }
     }
