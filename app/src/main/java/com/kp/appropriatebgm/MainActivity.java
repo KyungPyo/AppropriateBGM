81b2736d08340d544a2984d42b217e203b813eae
KyungPyo <leepyo116@gmail.com>
2016-02-23 20:49:52 +0900

    메인화면 로고 및 타이틀

--- app/src/main/java/com/kp/appropriatebgm/MainActivity.java
+++ app/src/main/java/com/kp/appropriatebgm/MainActivity.java
@@ -35,10 +35,9 @@ import com.kp.appropriatebgm.Category.CategoryActivity;
 import com.kp.appropriatebgm.DBController.BGMInfo;
 import com.kp.appropriatebgm.DBController.Category;
 import com.kp.appropriatebgm.DBController.DBManager;
-import com.kp.appropriatebgm.Setting.SettingActivity;
 import com.kp.appropriatebgm.DBController.Favorite;
+import com.kp.appropriatebgm.Setting.SettingActivity;
 import com.kp.appropriatebgm.favoritebgm.BGMListAdapter;
-import com.kp.appropriatebgm.favoritebgm.CategoryListAdapter;
 import com.kp.appropriatebgm.favoritebgm.CategoryListAdapterForMain;
 import com.kp.appropriatebgm.favoritebgm.FavoriteActivity;
 
@@ -57,9 +56,6 @@ public class MainActivity extends AppCompatActivity {
     private PlaybackBarTask playbackBarTask;
 
     // View 선언
-    private ImageView actionbarSearchBtn;
-    private ImageView actionbarLogo;
-    private EditText actionbarSearchTxt;
     private ViewGroup btnFileManage;
     private ViewGroup groupFileManage;
     private ViewGroup btnEditComplete;
@@ -106,7 +102,9 @@ public class MainActivity extends AppCompatActivity {
     //검색 부분
     private EditText editTextSearch;
     private ImageView searchButton;
-    boolean isVisible = false;
+    private boolean isVisible = false;
+    private ImageView imageLogo;
+    private ImageView imageTitle;
 
     //파일 삭제 시 Favorite 도 update 시켜야 하므로 필요
     private ArrayList<Favorite> favoriteList;
@@ -193,8 +191,10 @@ public class MainActivity extends AppCompatActivity {
         txtMaxTime = (TextView)findViewById(R.id.main_text_maxtime);
         txtPlayTime = (TextView)findViewById(R.id.main_text_playtime);
 
-        editTextSearch=(EditText)findViewById(R.id.main_editText_search);
-        searchButton=(ImageView)findViewById(R.id.main_btn_search);
+        editTextSearch = (EditText)findViewById(R.id.main_editText_search);
+        searchButton = (ImageView)findViewById(R.id.main_btn_search);
+        imageLogo = (ImageView)findViewById(R.id.main_image_logo);
+        imageTitle = (ImageView)findViewById(R.id.main_image_title);
 
 
         setSupportActionBar(toolbar);           // Toolbar를 액션바로 사용한다
@@ -363,10 +363,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (!isVisible) {
                     editTextSearch.setVisibility(View.VISIBLE);
+                    imageTitle.setVisibility(View.INVISIBLE);
                     isVisible = true;
                 } else {
                     final InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
                     editTextSearch.setVisibility(View.INVISIBLE);
+                    imageTitle.setVisibility(View.VISIBLE);
                     listItemCheckFree();
                     bgmListView.setAdapter(null);
                     bgmListView.setAdapter(bgmAdapter);
